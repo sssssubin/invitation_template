cc16e085eced252b026154e4887811635e86ac49
forest-k <bstnqls@naver.com>
2024-07-28 17:05:48 +0900

    feat: 비디오 최적화

--- src/components/sections/HeroMedia.tsx
+++ src/components/sections/HeroMedia.tsx
@@ -8,15 +8,11 @@ const cx = classNames.bind(styles)
 function MainImg() {
   return (
     <Section className={cx('container')}>
-      <img src="/assets/img-sample2.jpg" alt="샘플 이미지" />
-      {/* <video
-        autoPlay={true}
-        muted={true}
-        loop={true}
-        poster="/assets/poster.jpg"
-      >
+      {/* <img src="/assets/img-sample2.jpg" alt="샘플 이미지" /> */}
+      <video autoPlay loop muted poster="/assets/poster.jpg">
+        <source src="/assets/main.webm" type="video/webm" />
         <source src="/assets/main.mp4" type="video/mp4" />
-      </video> */}
+      </video>
     </Section>
   )
 }
