f58c192538d60778f54cc550a14fc3e12df907f4
forest-k <bstnqls@naver.com>
2024-08-11 17:21:48 +0900

    feat: errorboundary

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,6 @@
 import classNames from 'classnames/bind'
-import { useEffect, useState } from 'react'
 
 import styles from './App.module.scss'
-
-import FullScreenMessage from '@components/shared/FullScreenMessage'
 
 import HeroMedia from '@components/sections/HeroMedia'
 import ImageGallery from '@components/sections/ImageGallery'
@@ -20,11 +17,7 @@ import useWedding from './hooks/useWedding'
 const cx = classNames.bind(styles)
 
 function App() {
-  const { wedding, error } = useWedding()
-
-  if (error) {
-    return <FullScreenMessage type="error" />
-  }
+  const { wedding } = useWedding()
 
   if (wedding == null) {
     return null
